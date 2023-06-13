3f8532a695bf9501c25127a7ad1313f20517772d
shinomiya-akirawane <1226691698@qq.com>
2023-06-13 17:57:12 +0100

    add new features

--- pages/1 Engagement Visualization.py
+++ pages/1 Engagement Visualization.py
@@ -8,7 +8,7 @@ def get_bar_chart(data):
     chart = alt.Chart(data).mark_bar().encode(
         alt.X('date:T'),
         alt.Y('time_length:Q'),
-        color = alt.Color('type:N'),
+        color = alt.Color('type:N',scale = alt.Scale(domain=['HR hours', 'QoR completion','Active move moment hours','adjustChart'], range=['red', 'blue','green','white'])),
     )
     return chart.interactive(bind_y = False)
 
@@ -18,28 +18,20 @@ def get_point_chart(data):
     chart = alt.Chart(data).mark_circle().encode(
         alt.X('submit_datetime:T',),
         alt.Y('dot_height'),
-        color = alt.Color('type:N'),
+        color = alt.Color('type:N',scale = alt.Scale(domain=['HR hours', 'QoR completion','Active move moment hours','adjustChart'], range=['red', 'blue','green','white'])),
     )
     return chart.interactive()
 
 # page structure start here
 patient_id = st.number_input('patient id',min_value=1,max_value=42)
-with_zero = st.checkbox('Drop zero data')
+with_zero = st.checkbox('Show only active periods',value = True)
 
-if with_zero == True:
-    with open(os.path.join('.','df_data','Engagement_dot_30days_with_zero.pkl'),'rb') as f:
-        dot_df = pickle.load(f)
-        dot_df = dot_df.loc[(dot_df['patient_id'] == patient_id) ,:]
-    with open(os.path.join('.','df_data','Engagement_bar_30days_without_zero.pkl'),'rb') as f:
-        bar_df = pickle.load(f)
-        bar_df = bar_df.loc[(bar_df['patient_id'] == patient_id) ,:]
-else:
-    with open(os.path.join('.','df_data','Engagement_dot_30days_with_zero.pkl'),'rb') as f:
-        dot_df = pickle.load(f)
-        dot_df = dot_df.loc[(dot_df['patient_id'] == patient_id) ,:]
-    with open(os.path.join('.','df_data','Engagement_bar_30days_with_zero.pkl'),'rb') as f:
-        bar_df = pickle.load(f)
-        bar_df = bar_df.loc[(bar_df['patient_id'] == patient_id) ,:]
+with open(os.path.join('.','df_data','Engagement_dot_30days_with_zero.pkl'),'rb') as f:
+    dot_df = pickle.load(f)
+    dot_df = dot_df.loc[(dot_df['patient_id'] == patient_id) ,:]
+with open(os.path.join('.','df_data','Engagement_bar_30days_without_zero.pkl'),'rb') as f:
+    bar_df = pickle.load(f)
+    bar_df = bar_df.loc[(bar_df['patient_id'] == patient_id) ,:]
 
 chart = alt.layer(get_bar_chart(bar_df),get_point_chart(dot_df))
 st.altair_chart(chart,use_container_width=True)
--- pages/Behavior_Visualization.py
+++ pages/Behavior_Visualization.py
@@ -2,22 +2,49 @@ import streamlit as st
 import pickle
 import altair as alt
 import os
-
+import datetime 
 def get_point_chart(data):
     if len(data) < 0:
         return alt.LayerChart()
     chart = alt.Chart(data).mark_circle().encode(
-        alt.X('start_date:T'),
-        alt.Y('hoursminutes(start_time):T'),
-        color = alt.Color('type:N')
+        alt.X('start_date:T',title = 'date'),
+        alt.Y('hoursminutes(start_time):T',title = 'time of day'),
+        color = alt.Color('type:N', scale = alt.Scale(domain=['HR', 'qor15','step','adjustChart'], range=['red', 'blue','green','white']))
     )
     return chart.interactive(bind_y = False)
 
+def get_area_chart_1(chart_df):
+    chart = alt.Chart(chart_df).mark_rect().encode(
+        x = alt.value(0),
+        x2 = alt.value(2000),
+        y = alt.value(200),
+        y2 = alt.value(1000),
+        color = alt.ColorValue('grey')
+    )
+    return chart
+
+def get_area_chart_2(chart_df):
+    chart = alt.Chart(chart_df).mark_rect().encode(
+        x = alt.value(0),
+        x2 = alt.value(2000),
+        y = alt.value(0),
+        y2 = alt.value(30),
+        color = alt.ColorValue('grey')
+    )
+    return chart
 # page strcutrue start here
 patient_id = st.number_input('patient id',min_value=1,max_value=42)
 with_zero = st.checkbox('Show only active periods',value = True)
-displayElem = st.multiselect('Please choose heart rate or step or qor15',['heart rate','step','qor15'],default=None)
+displayElem = st.multiselect('Please choose heart rate or step or qor15',['heart rate','step','qor15'],default='qor15')
 graph_title = st.subheader('Data availability of NO.'+ str(patient_id))
+chart = alt.LayerChart()
+with open(os.path.join('.','df_data','behavior_visualization_colored_area.pkl'),'rb') as f:
+    chart_df = pickle.load(f)
+    chart_df = chart_df.loc[(chart_df['patient_id'] == patient_id) ,:]
+    del chart_df['patient_id']
+    del chart_df['rect_num']
+    chart = alt.layer(chart,get_area_chart_1(chart_df))
+    chart = alt.layer(chart,get_area_chart_2(chart_df))
 if with_zero == True:
     with open(os.path.join('.','df_data','Engagement_24_hours_date_without_zero.pkl'),'rb') as f:
         chart_df = pickle.load(f)
@@ -26,11 +53,12 @@ else:
     with open(os.path.join('.','df_data','Engagement_24_hours_date_with_zero.pkl'),'rb') as f:
         chart_df = pickle.load(f)
         chart_df = chart_df.loc[(chart_df['patient_id'] == patient_id) ,:]
-chart = alt.LayerChart()
 if 'heart rate' in displayElem:
     chart = alt.layer(chart,get_point_chart(chart_df.loc[(chart_df['type'] == 'HR'),:]))
 if 'step' in displayElem:
     chart = alt.layer(chart,get_point_chart(chart_df.loc[(chart_df['type'] == 'step'),:]))
 if 'qor15' in displayElem:
     chart = alt.layer(chart,get_point_chart(chart_df.loc[(chart_df['type'] == 'qor15'),:]))
+
+chart = alt.layer(chart,get_point_chart(chart_df.loc[(chart_df['type'] == 'adjustChart'),:]))
 st.altair_chart(chart,use_container_width=True)
